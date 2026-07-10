8dfe71806f51927f153cdef1fc1ef1837fea59d7
linus <lkronenb1@googlemail.com>
2026-07-10 22:19:32 +0200

    feat: Registering logs in directly

--- src/main/kotlin/com/example/springmodulithkickstart/user/AuthenticationController.kt
+++ src/main/kotlin/com/example/springmodulithkickstart/user/AuthenticationController.kt
@@ -1,10 +1,9 @@
 package com.example.springmodulithkickstart.user
 
-import com.example.springmodulithkickstart.user.api.LoginUserDto
-import com.example.springmodulithkickstart.user.api.RegisterUserDto
-import com.example.springmodulithkickstart.user.api.dto.LoginResponse
+import com.example.springmodulithkickstart.user.api.dto.LoginUserDto
+import com.example.springmodulithkickstart.user.api.dto.RegisterUserDto
+import com.example.springmodulithkickstart.user.api.dto.TokenResponseDto
 import com.example.springmodulithkickstart.user.infrastructure.AuthenticationService
-import com.example.springmodulithkickstart.user.infrastructure.db.User
 import com.example.springmodulithkickstart.user.infrastructure.jwt.JwtService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -19,16 +18,21 @@ class AuthenticationController(
     private val authenticationService: AuthenticationService
 ) {
     @PostMapping("/signup")
-    fun register(@RequestBody registerUserDto: RegisterUserDto): ResponseEntity<User> {
-        val registeredUser = authenticationService.signup(registerUserDto)
-        return ResponseEntity.ok(registeredUser)
+    fun register(@RequestBody registerUserDto: RegisterUserDto): ResponseEntity<TokenResponseDto> {
+        authenticationService.signup(registerUserDto)
+        val loginUserDto = LoginUserDto(registerUserDto.email, registerUserDto.password);
+        return authenticateWithLoginUserDto(loginUserDto);
     }
 
     @PostMapping("/login")
-    fun authenticate(@RequestBody loginUserDto: LoginUserDto): ResponseEntity<LoginResponse> {
+    fun authenticate(@RequestBody loginUserDto: LoginUserDto): ResponseEntity<TokenResponseDto> {
+        return authenticateWithLoginUserDto(loginUserDto);
+    }
+
+    fun authenticateWithLoginUserDto(loginUserDto: LoginUserDto): ResponseEntity<TokenResponseDto> {
         val authenticatedUser = authenticationService.authenticate(loginUserDto)
         val jwtToken = jwtService.generateToken(authenticatedUser)
-        val loginResponse = LoginResponse(token = jwtToken, expiresIn = jwtService.expirationTime)
+        val loginResponse = TokenResponseDto(token = jwtToken, expiresIn = jwtService.expirationTime)
         return ResponseEntity.ok(loginResponse)
     }
 }
