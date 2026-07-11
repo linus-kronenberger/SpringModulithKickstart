83ba9f886f37b12b0896fb9548c3fdd00f94f23e
linus <lkronenb1@googlemail.com>
2026-07-11 23:58:12 +0200

    feat: Added review entity with reviewer user

--- src/main/kotlin/com/example/springmodulithkickstart/review/infrastructure/db/Review.kt
+++ src/main/kotlin/com/example/springmodulithkickstart/review/infrastructure/db/Review.kt
@@ -1,5 +1,30 @@
 package com.example.springmodulithkickstart.review.infrastructure.db
 
+import com.example.springmodulithkickstart.user.infrastructure.db.User
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import java.io.Serializable
 
-class Review {
+@Entity
+class Review : Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    var id: String? = null
+
+    @Column(nullable = false)
+    var movieId: String? = null
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reviewer_id", nullable = false)
+    var reviewer: User? = null
+
+    var reviewText: String? = null
+
+    var rating: Int? = null
 }
